44157d9c9b01d0fedb3a10e10d53e70d1824425d
Alice Dias <100800760+dias0pe@users.noreply.github.com>
2023-05-07 22:47:23 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+public class main {
+    private String reminderText;
+  
+    public main (String reminderText) {
+      this.reminderText = reminderText;
+    }
+  
+    public String getReminderText() {
+      return reminderText;
+    }
+  
+    public void setReminderText(String reminderText) {
+      this.reminderText = reminderText;
+    }
+  }
